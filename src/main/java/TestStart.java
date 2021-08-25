cacfd4ea126063d96ed6bc02766697d2e1351c2e
skelon14 <86371252+skelon2014@users.noreply.github.com>
2021-08-25 16:21:09 +0300

    added class launchApp

--- src/main/java/TestStart.java
+++
@@ -1,9 +0,0 @@
-public class TestStart {
-    public static void main(String[] args) {
-        System.out.println("Hello 4uvak!");
-        System.out.println("Hello 4uvak!");
-        System.out.println("Hello 4uvak!");
-        System.out.println("Hello 4uvak!");
-
-    }
-}
