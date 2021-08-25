7c3f978cd65cb7f81765faa3288ae1909412837f
skelon14 <86371252+skelon2014@users.noreply.github.com>
2021-08-25 14:38:14 +0300

    3

--- src/main/java/TestStart.java
+++ src/main/java/TestStart.java
@@ -2,5 +2,6 @@ public class TestStart {
     public static void main(String[] args) {
         System.out.println("Hello 4uvak!");
         System.out.println("Hello 4uvak!");
+        System.out.println("Hello 4uvak!");
     }
 }
